d7a2fc4321c244b73b118637ff5a7403a224bccb
Diane10 <46956585+Diane10@users.noreply.github.com>
2020-12-04 21:14:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
 
 
 st.write(""" 
